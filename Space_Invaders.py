d8870458d095e28460745de866438899314182f5
Vivian <42046607+vivian238@users.noreply.github.com>
2018-10-11 01:40:05 -0400

    Create Space_Invaders.py

---
+++ Space_Invaders.py
@@ -0,0 +1,18 @@
+# Space Invaders (Classic Arcade Game Written in Python Using PyGame Module)
+# Code Written by Vivian
+
+# _________Import PyGame, Random, & Sys Libraries_________
+import pygame, sys
+from pygame.locals import *
+from random import shuffle
+
+# _________Define Some Colors_________
+#           R      G      B
+BLACK  = (  0,     0,     0)
+WHITE  = (255,   255,   255)
+GREEN  = (  0,   255,     0)
+YELLOW = (255,   255,     0)
+RED    = (255,     0,     0)
+PINK   = (250,   100,   250)
+PURPLE = (160,     0,   250)
+CYAN   = (  0,   255,   255)
